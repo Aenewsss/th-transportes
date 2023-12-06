f7c6a4870f7ca8ecc1ef2da310da4b101f89f76d
Aenewsss <62079043+Aenewsss@users.noreply.github.com>
2023-12-06 13:10:34 -0300

    ok

--- src/environments.ts
+++ src/environments.ts
@@ -1,5 +1,5 @@
 export const environments = {
-    whatsAppLink: "https://api.whatsapp.com/send?phone=556192485945&text=Olá, estou entrando em contato através do site e gostaria de saber mais sobre os serviços da TH Transportes!",
+    whatsAppLink: "https://api.whatsapp.com/send?phone=5561996439672&text=Olá, estou entrando em contato através do site e gostaria de saber mais sobre os serviços da TH Transportes!",
     mapsLink: "https://www.google.com/maps/dir/-15.866522,-47.8131019/Setor+Comercial+Norte+QUADRA+04+Centro+Empresarial+Varig+-+Asa+Norte,+Brasília+-+DF/@-15.7862021,-47.8894461,16.75z/data=!4m9!4m8!1m1!4e1!1m5!1m1!1s0x935a3afc07e6f489:0xd17861caa9aa1b85!2m2!1d-47.8871492!2d-15.7865593?entry=ttu",
     mailLink: "mailto:cotacao@iluminus.solar",
     locationLink: "https://www.google.com/maps?ll=-15.78454,-47.88937&z=17&t=m&hl=pt-BR&gl=BR&mapclient=embed"
