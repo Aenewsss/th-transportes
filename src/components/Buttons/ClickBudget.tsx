0f5d00a973c9f740ce59ca954a98401f96877f76
Aenewsss <62079043+Aenewsss@users.noreply.github.com>
2023-12-06 13:40:24 -0300

    ok

--- src/components/Buttons/ClickBudget.tsx
+++ src/components/Buttons/ClickBudget.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const ClickBudget = () => {
     return (
         <Link target="_blank" href={environments.whatsAppLink} className="btn btn-sec text-white scale fs-3 d-flex gap-2 justify-content-center align-items-center ps-5 pe-5">
-            <Image width={30} height={30} src="/icons/WhatsApp.svg" alt="whats icon" />
+            <Image width={30} height={30} src="/icons/whats-icon.png" alt="whats icon" />
             <span>Clique e Faça um Orçamento!</span>
         </Link>
     );
